8779b69cbfb78843551aaf37ef0219c7b6f30a2e
bilelsag <bilel.saghrouchni@gmail.com>
2025-08-04 10:40:03 +0200

    (update) CV file + tech stack

--- static/components/TechStack.tsx
+++ static/components/TechStack.tsx
@@ -12,17 +12,18 @@ const items: Item[] = [
   { label: 'Python',      src: '/img/stack/python.png',      href: 'https://www.python.org/' },
   { label: 'TensorFlow',  src: '/img/stack/tensorflow.png',  href: 'https://www.tensorflow.org/' },
   { label: 'Gymnasium',   src: '/img/stack/gym.png',   href: 'https://github.com/Farama-Foundation/Gymnasium' },
-  { label: 'ML-Flow',  src: '/img/stack/mlflow.png',  href: 'https://mlflow.org/' },
+  { label: 'Stable-baseline3',   src: '/img/stack/sb3.png',   href: 'https://github.com/DLR-RM/stable-baselines3/' },
   { label: 'Scikit Learn',  src: '/img/stack/sklearn.png',  href: 'https://github.com/scikit-learn/scikit-learn' },
   { label: 'Langchain',  src: '/img/stack/langchain.png',  href: 'https://github.com/langchain-ai/langchain' },
   { label: 'Streamlit',  src: '/img/stack/streamlit.png',  href: 'https://github.com/streamlit/streamlit' },
+  { label: 'ML-Flow',  src: '/img/stack/mlflow.png',  href: 'https://mlflow.org/' },
   { label: 'UV',  src: '/img/stack/uv.svg',  href: 'https://github.com/astral-sh/uv' },
   { label: 'pre-commit',  src: '/img/stack/precommit.png',  href: 'https://github.com/pre-commit/pre-commit'},
 ];
 
 export default function TechStack() {
   return (
-    <div className={styles.row} role="list" aria-label="Ma stack technique">
+    <div className={styles.row} role="list" aria-label="tech-stack">
       {items.map((it) => {
         const imgUrl = useBaseUrl(it.src);
         const Tag = it.href ? 'a' : 'div';
